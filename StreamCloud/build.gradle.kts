70e1cfeb97d1f04bd2c11c16752d69256eb696bd
ycngmn <41749571+ycngmn@users.noreply.github.com>
2025-02-04 21:56:09 +0100

    Add streamcloud.my
    completed extension request #1

---
+++ StreamCloud/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Stream Filme Kostenlos Online anschauen Deutsch."
+    authors = listOf("ycngmn")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("Movie","TvSeries")
+
+
+    // random cc logo i found
+    iconUrl = "https://streamcloud.my/templates/streamcloud/images/newlogo.svg"
+}
