173ac40dfff7d5d10ab98eaa6042d2f4c6f5a902
ycngmn <niloymollick90@gmail.com>
2025-05-25 21:08:31 +0200

    Added AnimeLuxe.org - Arabic

---
+++ AnimeLuxe/src/main/kotlin/com/ycngmn/AnimeLuxePlugin.kt
@@ -0,0 +1,12 @@
+package com.ycngmn
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class AnimeLuxePlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(AnimeLuxeProvider())
+    }
+}
