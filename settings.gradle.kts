9e3ddae4dcb8c04cb7b55392861f2f4b718d9575
samsonytb2 <sassoetien06@gmail.com>
2026-04-01 09:20:06 +0000

    Comment out project inclusion for MovieboxPK

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,4 +16,4 @@ fun File.eachDir(block: (File) -> Unit) {
 }
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+// include(":MovieboxPK")
